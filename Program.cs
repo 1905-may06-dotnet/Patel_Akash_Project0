d23d0047aef9f67a986637a0254142bc558531dd
Akash <akashmukeshpatel@outlook.com>
2019-05-07 10:07:57 -0700

    //initial project commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Patel_Akash_Project0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
